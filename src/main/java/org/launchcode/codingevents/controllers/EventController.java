4aa0fb8c34b8a05391f6c4ed91a712d7648eeb17
Nur Cetin <seymanur4450@gmail.com>
2020-10-05 14:00:37 -0500

    commit

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -17,12 +19,20 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
+//        model.addAttribute("events", events);
+//        return "events/index";
+
+        Map<String, String> events = new HashMap<>();
+        events.put("Code With Pride", "LGBT Friendly Coding Meetup");
+        events.put("Strange Loop", "Loops that are strange");
+        events.put("Apple WWDC", "Event for people who like overpriced computers");
         model.addAttribute("events", events);
+
         return "events/index";
     }
 
