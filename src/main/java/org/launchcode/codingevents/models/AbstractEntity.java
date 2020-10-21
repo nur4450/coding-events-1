e91e267ca33e2d9ebd65a8e800424a67e90a433a
Nur Cetin <seymanur4450@gmail.com>
2020-10-21 13:03:28 -0500

    Finished studio 17.5

---
+++ src/main/java/org/launchcode/codingevents/models/AbstractEntity.java
@@ -0,0 +1,45 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public AbstractEntity(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Event event = (Event) o;
+//        return id == event.id;
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity entity = (AbstractEntity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
